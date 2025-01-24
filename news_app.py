baec2f26cbb0fb6a026d662bedcb221555de434e
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-25 00:16:14 +0530

    news_app.py

--- news_app.py
+++ news_app.py
@@ -2,9 +2,17 @@
 # Streamlit deployment code
 import streamlit as st
 import pickle
-
+st.markdown(
+    """
+    <style>
+        .stProgress > div > div > div > div {
+            background-color: green;
+        }
+    </style>""",
+    unsafe_allow_html=True,
+)
 def main():
-    st.title("Fake and Real News Detection")
+    st.title("News Detection")
 
     # Load the model and vectorizer
     with open('random_forest_model.pkl', 'rb') as model_file:
@@ -15,8 +23,16 @@ def main():
 
     # User input
     user_input = st.text_area("Enter news text to analyze:")
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+
+    for percent_complete in range(100):
+        my_bar.progress(percent_complete + 1, text=progress_text)
+        time.sleep(0.1)
+        my_bar.empty()
 
     if st.button("Analyze"):
+        
         if user_input.strip():
             # Transform user input
             input_vectorized = vectorizer.transform([user_input])
