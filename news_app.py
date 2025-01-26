156acd133c4281027c845c49b8d959be0cc2b962
Aashif-k <ashif.aachi.k@gmail.com>
2025-01-27 00:24:34 +0530

    news_app.py

--- news_app.py
+++ news_app.py
@@ -3,9 +3,60 @@
 import streamlit as st
 import pickle
 import time
-
+text_css="""
+<style>
+[data-testid="stTextAreaRootElement"]{
+color: #FFFFFF;
+opacity: 0.5;
+}
+</style>
+"""
+top_css="""
+<style>
+[data-testid="stDecoration"]{
+background-image: url("https://unsplash.com/photos/FNFBN4-GdlQ/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MjB8fHdlYnNpdGUlMjBiYWNrZ3JvdW5kfGVufDB8fHx8MTczNzgzOTk0MXww&force=true&w=2400");
+opacity: 1.0;
+}
+</style>
+"""
+headind_css="""
+<style>
+[data-testid="stHeading"]{
+color: #000000;
+text-align: center;
+opacity: 1.0;
+}
+"""
+out_cs="""
+<style>
+[data-testid="stAlert"]{
+background-color: #A94A4A;
+color: #FFFFFF;
+opacity: 0.7;
+}
+</style>
+"""
+head_css="""
+<style>
+[data-testid="stHeader"]{
+background-image: url("https://unsplash.com/photos/FNFBN4-GdlQ/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MjB8fHdlYnNpdGUlMjBiYWNrZ3JvdW5kfGVufDB8fHx8MTczNzgzOTk0MXww&force=true&w=2400");
+opacity: 1.0;
+}
+</style>
+"""
+page_pg_img="""
+<style>
+[data-testid="stAppViewContainer"]{
+position: Relative;
+background-image: url("https://unsplash.com/photos/FNFBN4-GdlQ/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MjB8fHdlYnNpdGUlMjBiYWNrZ3JvdW5kfGVufDB8fHx8MTczNzgzOTk0MXww&force=true&w=2400");
+opacity: 1.0;
+width: 100%;
+height: 100%;
+background-size: auto auto;
+}
+"""
 def main():
-    st.title("News Detection")
+    st.title("NEWS DETECTION")
     with open('random_forest_model.pkl', 'rb') as model_file:
         model = pickle.load(model_file)
 
@@ -13,9 +64,9 @@ def main():
         vectorizer = pickle.load(vectorizer_file)
 
     # User input
-    user_input = st.text_area("Enter news text to analyze:")
-
-    if st.button("Analyze"):
+    user_input = st.text_area(" ",placeholder="enter the news to analyze",label_visibility="collapsed")
+    l,ll,lm,m,rm,lr,r = st.columns(7)
+    if m.button("Analyze"):
         progress_text = "Operation in progress. Please wait."
         my_bar = st.progress(0, text=progress_text)
         for percent_complete in range(100):
@@ -35,6 +86,12 @@ def main():
             st.error("This news is Fake.")
     else:
         st.warning("Please enter some text.")
+    st.markdown(head_css,unsafe_allow_html=True)
+    st.markdown(out_cs,unsafe_allow_html=True)
+    st.markdown(page_pg_img,unsafe_allow_html=True)
+    st.markdown(headind_css,unsafe_allow_html=True)
+    st.markdown(top_css,unsafe_allow_html=True)
+    st.markdown(text_css,unsafe_allow_html=True)
     st.markdown(
         """
         <style>
